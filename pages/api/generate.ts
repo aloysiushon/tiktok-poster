8551fbce80b6511e40d97f434c7b9cb42dd36a36
Aloysius Hon <aloysiushon@Aloysiuss-MacBook-Air.local>
2025-05-16 14:15:26 +0800

    Fix Puppeteer launch options for Heroku

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -37,16 +37,16 @@ export default async function handler(
     }
     ffmpeg.setFfmpegPath(ffmpegPath);
 
-    const localMacPath =
-      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-    const herokuPath = "/app/.apt/usr/bin/chromium-browser";
+    // const localMacPath =
+    //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+    // const herokuPath = "/app/.apt/usr/bin/chromium-browser";
 
-    function getChromePath() {
-      if (process.env.CHROME_PATH) return process.env.CHROME_PATH;
-      if (fs.existsSync(localMacPath)) return localMacPath;
-      if (fs.existsSync(herokuPath)) return herokuPath;
-      throw new Error("No suitable Chrome executable found");
-    }
+    // function getChromePath() {
+    //   if (process.env.CHROME_PATH) return process.env.CHROME_PATH;
+    //   if (fs.existsSync(localMacPath)) return localMacPath;
+    //   if (fs.existsSync(herokuPath)) return herokuPath;
+    //   throw new Error("No suitable Chrome executable found");
+    // }
 
     // const executablePath = getChromePath();
 
